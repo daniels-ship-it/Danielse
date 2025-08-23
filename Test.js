1ed9e32bf2aae61228bee9a1613c6cc1fb040dad
daniels-ship-it <nazwaanugrah6@gmail.com>
2025-08-23 10:59:27 +0700

    Add files via upload

--- Test.js
+++ Test.js
@@ -2,9 +2,28 @@ alert("hey")
 let nama = prompt("nama")
  alert("nama sampean " + nama )
  
-if(nama == "daniel"){
-  nama = alert("inilah!!")
-}
-else{
-  nama = alert("keroco")
+//if(nama == "daniel"){
+  //nama = alert("inilah!!")
+//}
+//else{
+  //nama = alert("keroco")
+//}
+
+switch (nama) {
+  case "daniel":
+    alert("inilah!!");
+    break;
+  case "nazwa":
+    alert("lah");
+    break;
+  case "anugrah":
+    console.log("apa cona")
+    break;
+  case (""):
+    alert("??? mana?");
+    console.log("cih")
+    break;
+  default:
+    alert("keroco");
+    console.log(nama)
 }
